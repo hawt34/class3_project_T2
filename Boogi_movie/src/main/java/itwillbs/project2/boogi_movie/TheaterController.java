f4f3bb298a6c215ecb78d9c034fd059f8e23fb84
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-04-19 17:48:39 +0900

    TheaterController 이름 수정

--- Boogi_movie/src/main/java/itwillbs/project2/boogi_movie/TheaterController.java
+++ Boogi_movie/src/main/java/itwillbs/project2/boogi_movie/TheaterController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class theaterController {
+public class TheaterController {
 	@RequestMapping("/TheaterMain") 
 	public String theaterMain() {
 		

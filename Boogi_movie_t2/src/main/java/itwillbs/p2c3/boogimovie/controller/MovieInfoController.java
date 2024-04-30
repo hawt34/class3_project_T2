d45025f0ac803baa73cd5ba26eeda7ebfb3df180
최진우 <ITWILL@DESKTOP-7JIVS4K>
2024-04-30 09:17:39 +0900

    영화 상세페이지 추가 및 맵핑

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/MovieInfoController.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/MovieInfoController.java
@@ -7,8 +7,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MovieInfoController {
 	@GetMapping("movieInfo1")
-	public String movieInfo() {
+	public String movieInfo1() {
 		
 		return "movie/movie_info_1";
 	}
+	
+	@GetMapping("movieInfo2")
+	public String movieInfo2() {
+		
+		return "movie/movie_info_2";
+	}
+	
+	@GetMapping("movieInfo3")
+	public String movieInfo3() {
+		
+		return "movie/movie_info_3";
+	}
+
+	@GetMapping("movieInfo4")
+	public String movieInfo4() {
+		
+		return "movie/movie_info_4";
+	}
+	
+	@GetMapping("movieInfo5")
+	public String movieInfo5() {
+		
+		return "movie/movie_info_5";
+	}
+
 }

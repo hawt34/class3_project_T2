838c33e6a2ac64bba22d014114fe8569bf8b7ac0
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-05-02 18:40:11 +0900

    getNoticelist controller 메서드 작성

---
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/mapper/NoticeMapper.java
@@ -0,0 +1,14 @@
+package itwillbs.p2c3.boogimovie.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import itwillbs.p2c3.boogimovie.vo.NoticeVO;
+
+@Mapper
+public interface NoticeMapper {
+	
+	List<NoticeVO> selectNoticeList(); 
+	
+}

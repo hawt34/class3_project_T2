2301264964365987263781e97679c85587b29512
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-05-08 16:19:29 +0900

    김민주 - theaterService수정

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/service/TheaterService.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/service/TheaterService.java
@@ -28,13 +28,13 @@ public class TheaterService {
 
 
 
-<<<<<<< HEAD
 	public List<TheaterFacilityVO> getFacility(TheaterFacilityVO facility) {
-		return mapper.selectFacility(facility);
-=======
+		
+		return mapper.selectFacility(facility);	
+	}
+	
 	public int getTheaterName(String theater_name) {
 		return mapper.selectTheaterName(theater_name);
->>>>>>> branch 'main' of https://github.com/hawt34/class3_project_T2.git
 	}
 	
 	

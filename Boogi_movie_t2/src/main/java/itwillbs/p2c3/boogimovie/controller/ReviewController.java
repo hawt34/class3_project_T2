5ec155a5bafdc93904539cb5141de05b4c42ed1e
최진우 <ITWILL@DESKTOP-7JIVS4K>
2024-05-08 18:08:56 +0900

    영화 리뷰 수정 및 삭제 추가

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/ReviewController.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/ReviewController.java
@@ -3,6 +3,7 @@ package itwillbs.p2c3.boogimovie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import itwillbs.p2c3.boogimovie.service.ReviewService;
@@ -14,7 +15,7 @@ public class ReviewController {
 	
 	
 	@PostMapping("reviewPro")
-	private String reviewInfo(Model model, ReviewVO review) {
+	public String reviewInfo(Model model, ReviewVO review) {
 	 
 		System.out.println("리뷰" + review);
 
@@ -25,4 +26,34 @@ public class ReviewController {
 	    return "redirect:/movieInfo?movie_num=" + movieNum;
 		
 	}
+	@PostMapping("reviewUpdate")
+	public String reviewUpdate(ReviewVO review2, Model model) {
+		//여기 팝업창이라서 팝업창을 클로즈함.
+		//System.out.println("리뷰업데이트 데이터 확인" + review2 ); 데이터 전송확인 완료 주석처리함.
+		int updateCount = service.updateReview(review2);
+		if(updateCount > 0) {
+			model.addAttribute("msg", "성공적으로 수정했습니다!");
+			return "error/close";
+		} else {
+			model.addAttribute("msg", "별점 및 관람평 수정실패!");
+			return "error/fail";
+		}
+					
+	}
+	
+	@GetMapping("deleteReview")
+	public String deleteReview(int review_id, Model model) {
+		System.out.println("삭제리뷰"+review_id);
+		int deleteCount = service.deleteReview(review_id);
+		if(deleteCount > 0) {
+			model.addAttribute("msg", "성공적으로 삭제했습니다!");
+			return "error/close";
+		} else {
+			model.addAttribute("msg", "삭제실패!");
+			return "error/fail";
+		}
+		
+	}
+
+
 }

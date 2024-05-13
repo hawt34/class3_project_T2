3ced05cd0596eceadb19b84ac834a5f01093af03
jyojyo <jyojyo@jyo-PC>
2024-05-14 01:27:56 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/hawt34/class3_project_T2.git
    into main

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/TicketingController.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/controller/TicketingController.java
@@ -311,27 +311,13 @@ public class TicketingController {
 			
 		return final_list;
 	}
-<<<<<<< HEAD
+
 	@GetMapping("payment_reservation")
 	public String paymentReserve() {
 		return "payment/payment_reservation";
 	}
 }
-=======
 
->>>>>>> branch 'main' of https://github.com/hawt34/class3_project_T2.git
-		
-		
-		
-		
-		
-		
-		
-	
-	
-	
 
-	
-	}	
 		
 		

7e93831dd4c886b39e8d7e5630aec8a01f07d0d6
최진우 <ITWILL@DESKTOP-7JIVS4K>
2024-05-14 09:49:58 +0900

    무비 컨트롤러 하나로 합체

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/mapper/MovieMapper.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/mapper/MovieMapper.java
@@ -7,11 +7,13 @@ import org.apache.ibatis.annotations.Mapper;
 import itwillbs.p2c3.boogimovie.vo.MovieGenreVO;
 import itwillbs.p2c3.boogimovie.vo.MovieVO;
 @Mapper
-public interface MovieInfoMapper {
+public interface MovieMapper {
 	List<MovieVO> selectMovieInfo();
 	MovieVO selectMovie(MovieVO movie);
 	List<MovieVO> selectMovieInfoLike();
 	List<MovieVO> selectMovieInfoAbc();
 	int selectMovieGenreNum(MovieGenreVO movieGenre);
 	List<MovieVO> selectMovieInfoGenre(int genre_num);
+	List<MovieVO> selectMovieFuture();//상영예정작 리스트
+	MovieVO selectFutureMovieInfo(MovieVO futureMovie);//상영예정작 상세보기
 }

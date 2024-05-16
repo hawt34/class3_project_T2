8f1ff8054a51f1bd81fc5ed421381ea861d65e0c
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-05-16 19:50:24 +0900

    결제 수정 중

--- Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/mapper/PaymentMapper.java
+++ Boogi_movie_t2/src/main/java/itwillbs/p2c3/boogimovie/mapper/PaymentMapper.java
@@ -2,7 +2,13 @@ package itwillbs.p2c3.boogimovie.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import itwillbs.p2c3.boogimovie.vo.MemberVO;
+
 @Mapper
 public interface PaymentMapper {
+
+	MemberVO getMember(MemberVO member);
+
+	int usePoint(MemberVO member);
 
 }
